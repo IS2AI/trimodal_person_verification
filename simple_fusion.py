03e0b0154cf95ebe42d294710f8d72f96b52fbab
abdrakh1 <madina.bulatovna@gmail.com>
2022-02-18 14:16:42 +0600

    edited simple fusion to save predictions

--- simple_fusion.py
+++ simple_fusion.py
@@ -2,17 +2,17 @@ import pandas as pd
 import numpy as np
 import argparse
 import os
-from tuneThreshold import tuneThresholdfromScore, ComputeErrorRates, ComputeMinDcf
+from tuneThreshold import tuneThresholdfromScore, ComputeErrorRates, ComputeMinDcf, recordPredictions
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--path_rgb', required=True, type=str, help='Path to the visual predictions')
 parser.add_argument('--path_wav', required=True, type=str, help='Path to the audio predictions')
 parser.add_argument('--path_thr', default="", type=str, help='Path to the thermal predictions')
+parser.add_argument('--save_path', required=True, type=str, help='Path for saving the predictions')
+parser.add_argument('--test_list', required=True, type=str, help='Evaluation list for which to save predictions')
 
-parser.add_argument('--path_out', required=True, type=str, help='Path to the write the result')
 
-
-def simple_fusion(rgb_scores_filename, thr_scores_filename, wav_scores_filename, path_out):
+def simple_fusion(rgb_scores_filename, thr_scores_filename, wav_scores_filename, save_path, test_list):
     rgb_data = pd.read_fwf(rgb_scores_filename, sep=' ', header=None)
     wav_data = pd.read_fwf(wav_scores_filename, sep=' ', header=None)
 
@@ -27,16 +27,19 @@ def simple_fusion(rgb_scores_filename, thr_scores_filename, wav_scores_filename,
     c_miss = 1
     c_fa = 1
 
-    _, eer, _, _, _ = tuneThresholdfromScore(mean_scores, true_labels, [1, 0.1])
+    result = tuneThresholdfromScore(mean_scores, true_labels, [1, 0.1])
 
     fnrs, fprs, thresholds = ComputeErrorRates(mean_scores, true_labels)
     mindcf, threshold = ComputeMinDcf(fnrs, fprs, thresholds, p_target, c_miss, c_fa)
 
-    output = open(os.path.join(path_out, 'simple_fusion_scores.txt'), 'w+')
-    output.write(f'EER: {eer:0.4f}, MinDCF: {mindcf:0.4f}')
-    print(f'EER: {eer:0.4f}, MinDCF: {mindcf:0.4f}')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    recordPredictions(mean_scores, result[4], save_path, test_list, True)
+
+    eer = result[1]
+    print('EER: {eer:0.4f}, MinDCF: {mindcf:0.4f}')
     return eer, mindcf
 
 
 args = parser.parse_args()
-simple_fusion(args.path_rgb, args.path_thr, args.path_wav, args.path_out)
+simple_fusion(args.path_rgb, args.path_thr, args.path_wav, args.save_path, args.test_list)
